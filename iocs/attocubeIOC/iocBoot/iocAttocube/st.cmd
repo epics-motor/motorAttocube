74df1515403866bb4d5669eb8aae5ea22c1ee244
kpetersn <kmpeters@anl.gov>
2019-04-04 14:11:33 -0500

    Improved the example IOC

--- iocs/attocubeIOC/iocBoot/iocAttocube/st.cmd
+++ iocs/attocubeIOC/iocBoot/iocAttocube/st.cmd
@@ -1,8 +1,5 @@
 #!../../bin/linux-x86_64/attocube
 
-## You may have to change attocube to something else
-## everywhere it appears in this file
-
 < envPaths
 
 cd "${TOP}"
@@ -11,11 +8,17 @@ cd "${TOP}"
 dbLoadDatabase "dbd/attocube.dbd"
 attocube_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadRecords("db/xxx.db","user=kpetersn")
-
 cd "${TOP}/iocBoot/${IOC}"
+
+## motorUtil (allstop & alldone)
+dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=attocube:")
+
+##
+< ANC150.cmd
+
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=kpetersn"
+## motorUtil (allstop & alldone)
+motorUtilInit("attocube:")
+
+# Boot complete
